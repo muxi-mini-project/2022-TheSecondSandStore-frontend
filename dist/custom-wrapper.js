c76aaf0e5e8bd3ff865598ca662d7d378e6ab2d5
Old-Second <994981540@qq.com>
2022-04-07 18:58:26 +0800

    4.7

--- dist/custom-wrapper.js
+++
@@ -1,2 +0,0 @@
-(qq["webpackJsonp"] = qq["webpackJsonp"] || []).push([["custom-wrapper"],[],[["./node_modules/@tarojs/mini-runner/dist/template/custom-wrapper.js","runtime","taro","vendors"]]]);
-//# sourceMappingURL=custom-wrapper.js.map
--- dist/custom-wrapper.qml
+++
@@ -1,4 +0,0 @@
-<import src="./base.qml" />
-  <block qq:for="{{i.cn}}" qq:key="sid">
-    <template is="tmpl_0_container" data="{{i:item,l:''}}" />
-  </block>
